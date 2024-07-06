8388e1d0f2780274617c21da56c1d36c6d0540d2
bOr1sBrTVa <ponomaryov50@bk.ru>
2024-07-06 17:10:07 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,8 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       # Если Instagram не предоставляет email, вы можете использовать другой уникальный идентификатор
       user.email = auth.info.email || "#{auth.uid}@instagram.com"
-      # user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name   # assuming the user model has a name
-      # # Если Instagram не предоставляет email, вы можете использовать другой уникальный идентификатор
-      # user.email = auth.info.email || "#{auth.uid}@instagram.com"
     end
   end
 end
