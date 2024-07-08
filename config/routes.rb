e2664595748c3e96a296be678a142733cfa0b086
Daniil Mironenko <dan.mironenko@gmail.com>
2024-07-08 14:50:45 +0300

    feat(admin): init route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
+  get 'admin/index'
   
   get "up" => "rails/health#show", as: :rails_health_check
 end
