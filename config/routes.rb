bb8981c947535b9d3cc8e2d64198f388e5be0d58
Daniil Mironenko <dan.mironenko@gmail.com>
2024-07-08 15:17:10 +0300

    feat(admin): simplify route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  get 'admin/index'
+  get 'admin', to: 'admin#index'
   
   get "up" => "rails/health#show", as: :rails_health_check
 end
