e49ced2d6d4505059f8535dcdaa45394acce4e62
Daniil Mironenko <dan.mironenko@gmail.com>
2024-07-12 19:50:51 +0300

    feat(admin-panel): uncomment some stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
- # devise :database_authenticatable, :registerable,
-       #  :recoverable, :rememberable, :validatable,
-      #   :omniauthable, omniauth_providers: %i[instagram]
+ devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :validatable
 
   include UserConcern
 
