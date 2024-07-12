13d6b5f8ce1cfcde374ab260ad69929fdc5f9ec9
Daniil Mironenko <dan.mironenko@gmail.com>
2024-07-12 19:54:07 +0300

    feat(admin-panel): add `/admin` path to the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do 
   root 'home#index'
   #devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users
   get 'auth/:provider/callback', to: 'sessions#create'
   get '/login', to: 'sessions#new'
   
-  get 'admin', to: 'admin#index'
   resources :users
 
   get "up" => "rails/health#show", as: :rails_health_check
   get 'sessions/create'
+
+  namespace :admin, constraints: AdminConstraint.new do
+    resources :users
+
+    root to: "users#index"
+  end
 end
