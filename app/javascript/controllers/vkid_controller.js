23bf26be58e01440e414965e48e4b5d63bedeea1
wheremylikes dev machine <dev@evrone.com>
2024-07-14 14:14:19 +0000

    Revert "Merge branch 'newfront' into vk_front_kseniia"
    This reverts commit 4dbfe1fbcb5bc41f95c8ab055660eda6a998d0c8, reversing
    changes made to 57258d5d360363e200b6a09b02cb9ec1cea32d05.

--- app/javascript/controllers/vkid_controller.js
+++ app/javascript/controllers/vkid_controller.js
@@ -8,18 +8,18 @@ export default class extends Controller {
   }
   connect() {
     console.log("Hello");
-    const vkidDataElement = document.getElementById('vkid-data');
-    const dataAttributes = JSON.parse(vkidDataElement.getAttribute('data-attributes'));
-    
-    const { code_challenge, state, scopes } = dataAttributes;
     
     const VKID = window.VKIDSDK;
         VKID.Config.init({
           app: '51989509', // Идентификатор приложения.
           redirectUrl: 'https://wheremylikes.com/auth/vkontakte/callback/', // Адрес для перехода после авторизации.
-          state: state, // Произвольная строка состояния приложения.
-          codeVerifier: code_challenge, // Верификатор в виде случайной строки. Обеспечивает защиту передаваемых данных.
-          scope: scopes // Список прав доступа, которые нужны приложению.
+          //state: "<%= @state %>", // Произвольная строка состояния приложения.
+          //codeVerifier: "<%= @code_challenge%>", // Верификатор в виде случайной строки. Обеспечивает защиту передаваемых данных.
+          //scope: '<%= @scopes %>' // Список прав доступа, которые нужны приложению.
+          state: 'df29figadjsd82',
+          codeVerifier: 'FzG+WDtlamaYFRXi83XYv47GnVhUXw5ipjCYGzjV9js=',
+          scope: 'vkid.personal_info friends wall'
+
         });
         
         // Получение кнопки из разметки.
