db14d5aa8ce1fb73331cd5f7e6fc00f092fe780e
Daniil Mironenko <dan.mironenko@gmail.com>
2024-07-16 17:06:09 +0300

    fix(metrics-panel-lost-commits): recover `HOST` env variable

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,7 @@ module Wheremylikes
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.hosts << ENV['HOST']
   end
 end
