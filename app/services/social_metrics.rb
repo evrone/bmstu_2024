d9e8ef27f798c1650815c0751c69a559d778ae13
Daniil Mironenko <dan.mironenko@gmail.com>
2024-07-17 21:06:40 +0300

    style(user-metrics): some style fixes

--- app/services/social_metrics.rb
+++ app/services/social_metrics.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Calculates user metrics
 class SocialMetrics
   attr_reader :post_metrics,
               :active_friends,
@@ -32,6 +35,7 @@ class SocialMetrics
   # Engagement score - average activity shown by each user
   def get_engagement_score(post)
     return 0 if @friends.empty?
+
     ((post[:likes].size + post[:comments].size).to_f / @friends.size * ENGAGEMENT_SCORE_FACTOR).round
   end
 
@@ -63,9 +67,9 @@ class SocialMetrics
   # Computes set of active and inactive friends
   # based on activity (likes and comments) on posts
   def compute_active_friends
-    active_friends_set = @posts.flat_map { |post|
+    active_friends_set = @posts.flat_map do |post|
       post[:likes] + post[:comments]
-    }.uniq
+    end.uniq
     @active_friends = active_friends_set & @friends
     @inactive_friends = @friends - @active_friends
   end
@@ -108,11 +112,11 @@ class SocialMetrics
   # in the form "comments:likes"
   def compute_comments_likes_ratio
     common_divisor = @total_comments.gcd(@total_likes)
-    if common_divisor.zero?
-      @comments_likes_ratio = '0:0'
-    else
-      @comments_likes_ratio = "#{@total_comments / common_divisor}:#{@total_likes / common_divisor}"
-    end
+    @comments_likes_ratio = if common_divisor.zero?
+                              '0:0'
+                            else
+                              "#{@total_comments / common_divisor}:#{@total_likes / common_divisor}"
+                            end
   end
 
   # Computes target ratio of total comments to total likes
@@ -123,22 +127,22 @@ class SocialMetrics
     target_comments = (@total_comments * TARGET_COMMENTS_RATIO).round
     target_likes = (@total_likes * TARGET_LIKES_RATIO).round
     common_divisor = target_comments.gcd(target_likes)
-    if common_divisor.zero?
-      @target_comments_likes_ratio = '0:0'
-    else
-      @target_comments_likes_ratio = "#{target_comments / common_divisor}:#{target_likes / common_divisor}"
-    end
+    @target_comments_likes_ratio = if common_divisor.zero?
+                                     '0:0'
+                                   else
+                                     "#{target_comments / common_divisor}:#{target_likes / common_divisor}"
+                                   end
   end
 
   # Computes audience score based on the ratio of active friends
   # to total friends
   # The score is scaled to a range of 0.0 to 10.0
   def compute_audience_score
-    if @friends.empty?
-      @audience_score = 0.0
-    else
-      @audience_score = (@active_friends.size.to_f / @friends.size * 10).round(1)
-    end
+    @audience_score = if @friends.empty?
+                        0.0
+                      else
+                        (@active_friends.size.to_f / @friends.size * 10).round(1)
+                      end
   end
 
   # Computes average engagement score across all posts
