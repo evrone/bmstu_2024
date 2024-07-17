6db2dc337b65f1e9d5d2758c430ad5eabce4f752
wheremylikes dev machine <dev@evrone.com>
2024-07-17 09:43:19 +0000

    fixed

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Wheremylikes
+module ProstoSite
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
@@ -18,7 +18,9 @@ module Wheremylikes
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
-
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies # Required for all session management
+    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -26,5 +28,9 @@ module Wheremylikes
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << ENV['BASE_HOST']
+    config.hosts << ENV['PROXY_HOST']
+    config.web_console.permissions = ENV['PROXY_IP']
+    config.hosts << "wheremylikes.com"
   end
 end
