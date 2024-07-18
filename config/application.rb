34dc8299ac613e86d4535218f50525f801b80e7c
wheremylikes dev machine <dev@evrone.com>
2024-07-18 10:04:28 +0000

    codestyle

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module ProstoSite
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
     config.session_store :cookie_store, key: '_interslice_session'
     config.middleware.use ActionDispatch::Cookies # Required for all session management
     config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
