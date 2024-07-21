ea2484c55d0f6977a623e9d73fa40fea64483c0f
wheremylikes dev machine <dev@evrone.com>
2024-07-21 17:27:42 +0000

    inserting metrics

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -27,4 +27,10 @@ class HomeController < ApplicationController
                                 grant_type: 'authorization_code'
                               })
   end
+
+  def show
+    @user = User.find(params[:id])
+    metrics_service = SocialMetricsService.new(@user)
+    @metrics = metrics_service.calculate_metrics
+  end
 end
