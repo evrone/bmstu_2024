a200b2b4e721740377c6ed7e4b762af5dc868902
Daniil Mironenko <dan.mironenko@gmail.com>
2024-07-21 19:49:22 +0300

    generate test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,41 @@
 # frozen_string_literal: true
 
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+user = User.create!(email: 'user@example.com', password: 'password')
+
+20.times do |i|
+  friend = Friend.create!(friend_id: i + 1, first_name: 'Name', last_name: 'Surname', image_url: "http://example.com/image#{i + 1}.jpg")
+  Relationship.create!(user:, friend:)
+end
+
+Post.create!(
+  post_id: 1,
+  date: Time.now.to_i,
+  image_url: 'http://example.com/image1.jpg',
+  count_likes: 5,
+  count_comments: 3,
+  likers: '[4, 2, 18, 16, 20]',
+  commentators: '[1, 4, 18]',
+  user:
+)
+
+Post.create!(
+  post_id: 2,
+  date: Time.now.to_i,
+  image_url: 'http://example.com/image2.jpg',
+  count_likes: 8,
+  count_comments: 6,
+  likers: '[2, 18, 5, 9, 12, 6, 10, 3]',
+  commentators: '[6, 2, 3, 1, 4, 5]',
+  user:
+)
+
+Post.create!(
+  post_id: 3,
+  date: Time.now.to_i,
+  image_url: 'http://example.com/image3.jpg',
+  count_likes: 3,
+  count_comments: 2,
+  likers: '[1, 4, 6]',
+  commentators: '[19, 15]',
+  user:
+)
