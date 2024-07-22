3f6ccaad173f005e56384d8837517ea036e3aa0c
Daniil Mironenko <dan.mironenko@gmail.com>
2024-07-22 19:04:19 +0300

    feat(save-metrics): update Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@
 
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :vk_uid, presence: true, uniqueness: true
 end
--- db/migrate/20240719143108_create_posts.rb
+++ db/migrate/20240719143108_create_posts.rb
@@ -3,17 +3,17 @@
 class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
-      t.integer :post_id
+      t.integer :vk_uid
       t.integer :date
       t.text :image_url
-      t.integer :count_likes
-      t.integer :count_comments
-      t.text :likers
-      t.text :commentators
+      t.integer 'likes', array: true
+      t.integer 'comments', array: true
       t.integer :engagement_score, null: true, default: nil
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
+
+    add_index :posts, :vk_uid, unique: true
   end
 end
