81cd05cd0c25786571b72a9a846cb4d060045ce7
6intez <chep236@gmail.com>
2024-07-22 14:02:49 +0300

    change refresh tokens

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -25,31 +25,34 @@ class SessionsController < ApplicationController
       accessToken = response['access_token']
       refreshToken = response['refresh_token']
       deviceId = params['device_id']
-    # поиск пользователя или создание его
+      # поиск пользователя или создание его
       user = User.find_or_create_by(user_id: @userId) do |u|
         u.access_token = accessToken
         u.refresh_token = refreshToken
         u.user_device_id = deviceId
         u.access_token_expiration_time = Time.now + 55.minutes
       end
-    #проверка создания нового пользователя (если не создали то обновляем)
+      # проверка создания нового пользователя (если не создали то обновляем)
       if user.persisted?
-        user.update(access_token: accessToken, refresh_token: refreshToken, access_token_expiration_time: Time.now + 55.minutes, user_device_id: deviceId)
+        user.update(access_token: accessToken, refresh_token: refreshToken,
+                    access_token_expiration_time: Time.now + 55.minutes, user_device_id: deviceId)
       end
       sign_in user
       @user_is_signed_in = user_signed_in?
       redirect_to '/sessions/view'
     end
   end
+
   def view
     if user_signed_in?
       if needs_refresh_token?
         response_from_refresh = refresh_userTokens(current_user.refresh_token, current_user.device_id, params['state'])
-        user.update(access_token: response_from_refresh['access_token'], refresh_token: response_from_refresh['refresh_token'], access_token_expiration_time: Time.now + 55.minutes)
+        user.update(access_token: response_from_refresh['access_token'],
+                    refresh_token: response_from_refresh['refresh_token'], access_token_expiration_time: Time.now + 55.minutes)
       end
-        vk_client = VkApiClient.new(@current_user.refresh_token, @current_user.access_token, @current_user.user_id)
-        profile_data = vk_client.get_profile_info()
-        @user_name = profile_data['first_name'] + " " + profile_data["last_name"]
+      vk_client = VkApiClient.new(@current_user.refresh_token, @current_user.access_token, @current_user.user_id)
+      profile_data = vk_client.get_profile_info
+      @user_name = profile_data['first_name'] + ' ' + profile_data['last_name']
     else
       redirect_to root_path
     end
@@ -57,10 +60,10 @@ class SessionsController < ApplicationController
 
   def user_sign_out
     if user_signed_in?
-    sign_out @current_user
-    redirect_to root_path
+      sign_out @current_user
+      redirect_to root_path
     else
-      puts("no user founded")
+      puts('no user founded')
       redirect_to root_path
     end
   end
@@ -85,10 +88,12 @@ class SessionsController < ApplicationController
                          })
     response.body
   end
+
   def needs_refresh_token?
-    Time.now > @current_user.access_token_expiration_time 
+    Time.now > @current_user.access_token_expiration_time
   end
-  def refresh_userTokens(refresh_token,device_id,state)
+
+  def refresh_userTokens(refresh_token, device_id, state)
     conn = Faraday.new(url: 'https://id.vk.com') do |conn_builder|
       conn_builder.headers['Content-Type'] = 'application/json'
       conn_builder.headers['Accept'] = 'application/json'
@@ -99,8 +104,9 @@ class SessionsController < ApplicationController
 
     response = conn.post('/oauth2/auth', {
                            grant_type: 'refresh_token',
-                           device_id: device_id,
-                           state: state,
+                           refresh_token:,
+                           device_id:,
+                           state:,
                            client_id: '51989509'
                          })
     response.body
