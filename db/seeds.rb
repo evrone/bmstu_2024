0bbccd2134350e48ea777c1e407f0fb401c3e0c7
Daniil Mironenko <dan.mironenko@gmail.com>
2024-07-22 19:09:12 +0300

    update test data generation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,41 +1,41 @@
 # frozen_string_literal: true
 
-user = User.create!(email: 'user@example.com', password: 'password')
+Post.destroy_all
+Relationship.destroy_all
+Friend.destroy_all
+Metric.destroy_all
+User.destroy_all
+
+user = User.create!(email: 'test@example.com', password: 'password')
 
 20.times do |i|
-  friend = Friend.create!(friend_id: i + 1, first_name: 'Name', last_name: 'Surname', image_url: "http://example.com/image#{i + 1}.jpg")
+  friend = Friend.create!(vk_uid: i + 1, first_name: 'Name', last_name: 'Surname', image_url: "https://example.com/image#{i + 1}.jpg")
   Relationship.create!(user:, friend:)
 end
 
 Post.create!(
-  post_id: 1,
+  vk_uid: 1,
   date: Time.now.to_i,
-  image_url: 'http://example.com/image1.jpg',
-  count_likes: 5,
-  count_comments: 3,
-  likers: '[4, 2, 18, 16, 20]',
-  commentators: '[1, 4, 18]',
+  image_url: 'https://example.com/image1.jpg',
+  likes: [4, 2, 18, 16, 20],
+  comments: [1, 4, 18],
   user:
 )
 
 Post.create!(
-  post_id: 2,
+  vk_uid: 2,
   date: Time.now.to_i,
-  image_url: 'http://example.com/image2.jpg',
-  count_likes: 8,
-  count_comments: 6,
-  likers: '[2, 18, 5, 9, 12, 6, 10, 3]',
-  commentators: '[6, 2, 3, 1, 4, 5]',
+  image_url: 'https://example.com/image2.jpg',
+  likes: [2, 18, 5, 9, 12, 6, 10, 3],
+  comments: [6, 2, 3, 1, 4, 5],
   user:
 )
 
 Post.create!(
-  post_id: 3,
+  vk_uid: 3,
   date: Time.now.to_i,
-  image_url: 'http://example.com/image3.jpg',
-  count_likes: 3,
-  count_comments: 2,
-  likers: '[1, 4, 6]',
-  commentators: '[19, 15]',
+  image_url: 'https://example.com/image3.jpg',
+  likes: [1, 4, 6],
+  comments: [19, 15],
   user:
 )
