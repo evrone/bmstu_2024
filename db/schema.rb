f22a17f19a0bb9ebeb6b97ded92c264c6affdd13
Daniil Mironenko <dan.mironenko@gmail.com>
2024-07-22 19:08:37 +0300

    feat(save-metrics): update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -17,18 +17,19 @@ ActiveRecord::Schema[7.1].define(version: 20_240_719_150_646) do
   enable_extension 'plpgsql'
 
   create_table 'friends', force: :cascade do |t|
-    t.integer 'friend_id'
+    t.integer 'vk_uid'
     t.text 'first_name'
     t.text 'last_name'
     t.text 'image_url'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.index ['vk_uid'], name: 'index_friends_on_vk_uid', unique: true
   end
 
   create_table 'metrics', force: :cascade do |t|
     t.integer 'average_likes'
     t.integer 'target_likes'
-    t.integer 'average_comments'
+    t.integer 'average_cosmments'
     t.integer 'target_comments'
     t.string 'comments_likes_ratio'
     t.string 'target_comments_likes_ratio'
@@ -41,26 +42,29 @@ ActiveRecord::Schema[7.1].define(version: 20_240_719_150_646) do
   end
 
   create_table 'posts', force: :cascade do |t|
-    t.integer 'post_id'
+    t.integer 'vk_uid'
     t.integer 'date'
     t.text 'image_url'
-    t.integer 'count_likes'
-    t.integer 'count_comments'
-    t.text 'likers'
-    t.text 'commentators'
+    t.integer 'likes', array: true
+    t.integer 'comments', array: true
     t.integer 'engagement_score'
     t.bigint 'user_id', null: false
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
     t.index ['user_id'], name: 'index_posts_on_user_id'
+    t.index ['vk_uid'], name: 'index_posts_on_vk_uid', unique: true
   end
 
   create_table 'relationships', force: :cascade do |t|
-    t.integer 'user_id'
-    t.integer 'friend_id'
-    t.boolean 'is_active', default: true
+    t.bigint 'user_id'
+    t.bigint 'friend_id'
+    t.boolean 'active', default: true
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.index %w[friend_id user_id], name: 'index_relationships_on_friend_id_and_user_id'
+    t.index ['friend_id'], name: 'index_relationships_on_friend_id'
+    t.index %w[user_id friend_id], name: 'index_relationships_on_user_id_and_friend_id'
+    t.index ['user_id'], name: 'index_relationships_on_user_id'
   end
 
   create_table 'users', force: :cascade do |t|
