4326d02c794546a33258977a61b9fcd83a66dcc0
6intez <chep236@gmail.com>
2024-07-22 13:22:44 +0300

    refresh tokens

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -24,15 +24,17 @@ class SessionsController < ApplicationController
       @userId = response['user_id'].to_i
       accessToken = response['access_token']
       refreshToken = response['refresh_token']
+      deviceId = params['device_id']
     # поиск пользователя или создание его
       user = User.find_or_create_by(user_id: @userId) do |u|
         u.access_token = accessToken
         u.refresh_token = refreshToken
+        u.user_device_id = deviceId
         u.access_token_expiration_time = Time.now + 55.minutes
       end
     #проверка создания нового пользователя (если не создали то обновляем)
       if user.persisted?
-        user.update(access_token: accessToken, refresh_token: refreshToken, access_token_expiration_time: Time.now + 55.minutes)
+        user.update(access_token: accessToken, refresh_token: refreshToken, access_token_expiration_time: Time.now + 55.minutes, user_device_id: deviceId)
       end
       sign_in user
       @user_is_signed_in = user_signed_in?
@@ -42,13 +44,12 @@ class SessionsController < ApplicationController
   def view
     if user_signed_in?
       if needs_refresh_token?
-      #переход в другой контроллер (надо обратиться к vk_sdk)
-      else
-        puts(current_user.access_token)
+        response_from_refresh = refresh_userTokens(current_user.refresh_token, current_user.device_id, params['state'])
+        user.update(access_token: response_from_refresh['access_token'], refresh_token: response_from_refresh['refresh_token'], access_token_expiration_time: Time.now + 55.minutes)
+      end
         vk_client = VkApiClient.new(@current_user.refresh_token, @current_user.access_token, @current_user.user_id)
         profile_data = vk_client.get_profile_info()
         @user_name = profile_data['first_name'] + " " + profile_data["last_name"]
-      end
     else
       redirect_to root_path
     end
@@ -86,5 +87,22 @@ class SessionsController < ApplicationController
   end
   def needs_refresh_token?
     Time.now > @current_user.access_token_expiration_time 
+  end
+  def refresh_userTokens(refresh_token,device_id,state)
+    conn = Faraday.new(url: 'https://id.vk.com') do |conn_builder|
+      conn_builder.headers['Content-Type'] = 'application/json'
+      conn_builder.headers['Accept'] = 'application/json'
+      conn_builder.request :json
+      conn_builder.response :json
+      conn_builder.response :logger
+    end
+
+    response = conn.post('/oauth2/auth', {
+                           grant_type: 'refresh_token',
+                           device_id: device_id,
+                           state: state,
+                           client_id: '51989509'
+                         })
+    response.body
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_22_074428) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_22_100835) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_22_074428) do
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
     t.datetime "access_token_expiration_time"
+    t.string "user_device_id"
   end
 
 end
