f6be4332c08749820d74e7026d01a4728f614be6
Daniil Mironenko <dan.mironenko@gmail.com>
2024-07-22 19:05:59 +0300

    feat(save-metrics): update Relationship model

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -4,9 +4,5 @@ class Relationship < ApplicationRecord
   belongs_to :user
   belongs_to :friend
 
-  validates :is_active, inclusion: { in: [true, false] }
-
-  def self.update_activity(value, user, friend_ids)
-    where(user_id: user.id, friend_id: friend_ids).update_all(is_active: value)
-  end
+  validates :active, inclusion: { in: [true, false] }
 end
--- db/migrate/20240719145041_create_relationships.rb
+++ db/migrate/20240719145041_create_relationships.rb
@@ -3,11 +3,14 @@
 class CreateRelationships < ActiveRecord::Migration[7.1]
   def change
     create_table :relationships do |t|
-      t.integer :user_id
-      t.integer :friend_id
-      t.boolean :is_active, default: true
+      t.belongs_to :user
+      t.belongs_to :friend
+      t.boolean :active, default: true
 
       t.timestamps
+
+      t.index %i[user_id friend_id]
+      t.index %i[friend_id user_id]
     end
   end
 end
