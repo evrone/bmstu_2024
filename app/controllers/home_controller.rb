0f86fe65a94b18dff4320f65a64499322e75985b
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2024-07-24 23:18:56 +0300

    fix traefik and codestyle

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,12 @@
 
 class HomeController < ApplicationController
   def index
-    unless current_user
-      pkce_challenge = PkceChallenge.challenge
-      session[:code_verifier] = pkce_challenge.code_verifier
-      @data_attributes = {
-        challenge: pkce_challenge.code_challenge, state: session.id.public_id
-      }.to_json
-    end
+    return if current_user
+
+    pkce_challenge = PkceChallenge.challenge
+    session[:code_verifier] = pkce_challenge.code_verifier
+    @data_attributes = {
+      challenge: pkce_challenge.code_challenge, state: session.id.public_id
+    }.to_json
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -8,23 +8,24 @@ class Post < ApplicationRecord
   def self.from_vk(payload, owner)
     puts 'PAYLOAD!!!'
     puts payload
-    existing_post= find_or_create_by(vk_uid: payload[:post_id]) do |post|
+    existing_post = find_or_create_by(vk_uid: payload[:post_id]) do |post|
       # post.vk_uid = payload[:post_id],
       post.date =  payload[:date],
-      post.image_url = payload[:image_url],
-      # post.count_likes = payload[:count_likes],
-      # post.count_comments = payload[:count_comments],
-      post.likes = payload[:likers],
-      post.comments = payload[:commentators]
+                   post.image_url = payload[:image_url],
+                   # post.count_likes = payload[:count_likes],
+                   # post.count_comments = payload[:count_comments],
+                   post.likes = payload[:likers],
+                   post.comments = payload[:commentators]
       post.user = owner
     end
     existing_post.update(
-            date: payload[:date],
-            image_url: payload[:image_url],
-            # count_likes: payload[:count_likes],
-            # count_comments: payload[:count_comments],
-            likes: payload[:likers],
-            comments: payload[:commentators])
+      date: payload[:date],
+      image_url: payload[:image_url],
+      # count_likes: payload[:count_likes],
+      # count_comments: payload[:count_comments],
+      likes: payload[:likers],
+      comments: payload[:commentators]
+    )
     existing_post
   end
 end
