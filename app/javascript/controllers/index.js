e1ccef5c02685668c43fc95cf5829f2477b9dd6f
Mikhail Geine <mike.geine@gmail.com>
2024-07-24 14:09:00 +0300

    fix deploy

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,15 +1,15 @@
 // Import and register all your controllers from the importmap under controllers/*
 
-import { application } from "./application"
-import HelloController from "./hello_controller"
-import VkidController from "./vkid_controller"
+import { application } from "controllers/application"
+// import HelloController from "./hello_controller"
+// import VkidController from "./vkid_controller"
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
-// import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-// eagerLoadControllersFrom("controllers", application)
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+eagerLoadControllersFrom("controllers", application)
 
-application.register("hello", HelloController)
-application.register("vkid", VkidController)
+// application.register("hello", HelloController)
+// application.register("vkid", VkidController)
 
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
